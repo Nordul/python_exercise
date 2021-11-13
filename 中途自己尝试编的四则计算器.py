146a473ea3b6d30c17968417cb346139a2b23a86
Nordul <2200944584@qq.com>
2021-11-13 19:43:08 +0800

    计算器

--- 中途自己尝试编的四则计算器.py
+++ 中途自己尝试编的四则计算器.py
@@ -1,21 +1,21 @@
-x=input('输入第一个数字')
-x=int(x)
-y=input('输入第二个数字')
-y=int(y)
-n=input('输入运算符,1为加，2为减，3为乘，4为除:\n')
-n=int(n)
+x = input('输入第一个数字')
+x = int(x)
+y = input('输入第二个数字')
+y = int(y)
+n = input('输入运算符,1为加，2为减，3为乘，4为除:\n')
+n = int(n)
 
-if n==1:
+if n == 1:
     print(x+y)
     exit(0)
-if n==2:
+if n == 2:
     print(x-y)
     exit(0)
-if n==3:
+if n == 3:
     print(x*y)
     exit(0)
-if n==4:
+if n == 4:
     print(x/y)
     exit(0)
-if n!=1 or n!=2 or n!=3 or n!=4:
+if n != 1 or n != 2 or n != 3 or n != 4:
     print('运算符输入有误')
