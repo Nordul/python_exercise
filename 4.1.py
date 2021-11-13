39ca180d553f0d3e21c4af0e3cd60d06bdbde3a3
Nordul <2200944584@qq.com>
2021-11-13 17:02:48 +0800

    test

---
+++ 4.1.py
@@ -0,0 +1,4 @@
+pizza=['美式披萨','意式披萨','那不勒斯披萨']
+for like_pizza in pizza:
+    print(f'i like {like_pizza}')
+print('I really love pizza!')
---
+++ 4.3.py
@@ -0,0 +1,12 @@
+#num = range(1,1000001)
+#print(min(num))
+#print(max(num))
+#print(sum(num))
+
+#num = range(1,21,2)
+#for num1 in num:
+#    print(num1)
+
+
+num=[value**3 for value in range(1,11)]
+print(num)
