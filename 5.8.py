41641b3d8422ec9931bd8b81f29c47f3c66074b3
Nordul <2200944584@qq.com>
2021-11-18 07:46:05 +0800

    第二次提交

--- 5.8.py
+++ 5.8.py
@@ -1,6 +1,6 @@
 users = ['nordul','admin','lendul','bei cheng','bai ming']
 user = input('输入用户名')
 if user in users:
-    print(f'welcome {user.title()}')
+    print(f'Welcome {user.title()}')
 else:
     print('No such user')
